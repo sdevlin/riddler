25ba7bc13a26be1a119bd84b3363c070c8627737
Sean Devlin <devlin@alumni.nd.edu>
2014-09-25 09:35:03 -0500

    added gitignore, __init__.py, sha256 skeleton

---
+++ sha256.py
@@ -0,0 +1,16 @@
+class SHA256(object):
+    def __init__(self):
+        pass
+
+    def compress(self):
+        pass
+
+    def update(self, m):
+        pass
+
+    def final(self):
+        pass
+
+    @staticmethod
+    def digest():
+        pass
